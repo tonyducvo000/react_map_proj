36f8c6819453737d06f301ec29ce6aac3af41660
tony <tonyducvo000@gmail.com>
2021-07-10 18:51:49 -0700

    fixed map issue for test branch

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -4,6 +4,7 @@ import RobotDataDisplay from './robotDataDisplay';
 import ProductInfoDisplay from './productInfoDisplay';
 import StateDisplay from './stateDisplay'
 import convertTime from './helper/convertTime'
+//import Markers from './markers'
 
 import database from './database_cocos.json';
 
@@ -202,11 +203,18 @@ class MapContainer extends Component {
                         }
                     >
 
+                        {/* 
+                        <Markers
+                            base_keys_robots={base_keys_robots}
+                            base_values_robots={base_values_robots}
+                        >
+                        </Markers> */}
+
                         {
                             R.zip(base_keys_robots, base_values_robots).map((data) => {
 
                                 return (
-                                    <Marker label={data[0]} title={data[0]} onClick={this.parseAndHandleClick}
+                                    <Marker key={data[0]} label={data[0]} title={data[0]} onClick={this.parseAndHandleClick}
                                         position={{ lat: data[1].location.latitude, lng: data[1].location.longitude }} />
                                 )
                             })
