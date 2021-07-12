6e7f1df6ef6a4126b3a1f5bf56c192d77259ea3c
tony <tonyducvo000@gmail.com>
2021-07-12 00:03:01 -0700

    rewrite <StateDisplay> elegantly

--- src/components/stateDisplay.jsx
+++ src/components/stateDisplay.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 
 
 const StateDisplay = (props) => {
-    const { robotState, robotStateStore } = props;
+    const { robotState, robotStateStore, base_keys_robots } = props;
 
     return (
 
         <div className="stateDisplay">
 
-            <p><b>Robot D01</b> current state: {robotState.D01} - {robotStateStore[robotState.D01]}</p>
-            <p><b>Robot D02</b> current state: {robotState.D02} - {robotStateStore[robotState.D02]}</p>
-            <p><b>Robot D03</b> current state: {robotState.D03} - {robotStateStore[robotState.D03]}</p>
-            <p><b>Robot D04</b> current state: {robotState.D04} - {robotStateStore[robotState.D04]}</p>
-            <p><b>Robot D05</b> current state: {robotState.D05} - {robotStateStore[robotState.D05]}</p>
+            {base_keys_robots.map((keys) => {
+                return (
+                    <p><b>Robot {keys}</b> current state: {robotState[keys]} - {robotStateStore[robotState[keys]]}</p>
+                )
+
+            })}
+
         </div>
 
     );
